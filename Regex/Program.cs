b84eb87e4e07f248cfc7fe5dc49265d2959b5768
goyallucky <luckygoyal221982@gmail.com>
2026-01-20 01:40:21 +0530

    [Lucky] Add Program.cs file to execute all programs

---
+++ Regex/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Regex;
+using FilterEmployees;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // EmployeeMain.Start(); 
+
+
+        // ValidateUsername.Start();
+        // LicensePlateValidator.Start();
+        // HexColorValidator.Start();
+        // EmailExtractor.Start();
+        // CapitalizedWordExtractor.Start();
+        // ReplaceMultipleSpaces.Start();
+        // CensorBadWords.Start();
+        // DateExtractor.Start();
+        // ExtractCurrency.Start();
+        // ExtractProgramingLanguage.Start();
+        // LinkExtractor.Start();
+        // RepeatingWords.Start();
+        // ValidateCreditCard.Start();
+        // ValidateSecurityNumber.Start();
+        ValidateIPAddress.Start();
+    }
+}
