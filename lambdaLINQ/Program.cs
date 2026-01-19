add73979d5589c2a672b1d815ca4052b3e3e1197
goyallucky <luckygoyal221982@gmail.com>
2026-01-19 15:22:06 +0530

    [Lucky Goyal] Add Program.cs file to execute all the programs

---
+++ lambdaLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lambdaLINQ;
+using EmployeeFiltering;
+using Product;
+using StudentSorting;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // LINQMain.Start();
+        // EmployeeMain.Start();
+        // SortString.Start();
+        // ProductMain.Start();
+        StudentMain.Start();
+    }
+}
